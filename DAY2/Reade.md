f08d968a126af22c031d5dc40c4c64200164dd72
Nitish kumar <iamnitish405@gmail.com>
2025-06-18 17:08:22 +0530

    first commit

---
+++ DAY2/Reade.md
@@ -0,0 +1,12 @@
+  let num = 10;
+
+  primitive data type
+   let num = number
+             String
+             boolean
+             Bigint
+             null 
+             undefined
+             symbol
+ note : num is a name of memory location 
+  number = 20,34,56 23.43, 3.14  store kar sakte hai.
---
+++ DAY2/first.js
@@ -0,0 +1,43 @@
+// declare the variable 
+// let num = 30;
+// console.log(num);
+//  primitive data type
+
+// Number,boolean, string , undefined,null symbol, bigint
+
+
+// Number
+let account_number = 3946258657;
+console.log(typeof account_number);
+
+// String
+let str = "Nitish Bijendra";
+console.log(str);
+console.log( typeof str);
+
+
+// boolean
+
+let papa_ko_block_kar_rakha_hai = true;
+console.log(papa_ko_block_kar_rakha_hai);
+
+
+// undefined
+let my_age 
+console.log(my_age);
+
+// null concept
+let my_name = null;
+console.log(my_name);
+console.log(typeof my_name); //null is an object in javascript
+
+// bigint
+
+let a = 1234764829716473723457765432n;
+console.log(a);
+console.log(typeof a);
+console.log(Number.MAX_SAFE_INTEGER);
+console.log(Number.MIN_SAFE_INTEGER);
+
+// Non primitive data type 
+// Array , object , function , date
